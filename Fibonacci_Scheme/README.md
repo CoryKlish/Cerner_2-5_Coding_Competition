7519ec05aac9025cada446ce3bd4f0598557b81e
Cory Klish <Cory.Klish@cerner.com>
2019-09-17 11:29:15 -0400

    Scheme_Recursion

---
+++ Fibonacci_Scheme/README.md
@@ -0,0 +1,2 @@
+Fibonacci Sequence calculator using Scheme and Tail Recursion
+Run it at https://repl.it/@CoryKlish/fibscheme
---
+++ Fibonacci_Scheme/fibonacci.scm
@@ -0,0 +1,11 @@
+; cerner_2^5_2019
+; Fibonacci Sequence
+; Calculates the nth Fibonacci number using Tail Recursion
+; Pass 'a' as 0 (First Fibonacci number)
+; Pass 'b' as 1 (Second Fibonacci number)
+; To calculate 50th Fibonacci number: '(fib 50 0 1)'
+
+(define (fib n a b)
+  (cond ((<= n 0) a)
+        ((= n 1) b)
+        (else (fib (- n 1) b (+ a b)))
