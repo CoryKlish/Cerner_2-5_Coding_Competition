d5ce29698f99adb7bd5790425a9691b487cb9571
Cory Klish <Cory.Klish@cerner.com>
2019-09-17 11:52:43 -0400

    Getting rid of args

--- Fibonacci_Scheme/fibonacci.scm
+++ Fibonacci_Scheme/fibonacci.scm
@@ -1,11 +1,14 @@
 ; cerner_2^5_2019
+
 ; Fibonacci Sequence
 ; Calculates the nth Fibonacci number using Tail Recursion
-; Pass 'a' as 0 (First Fibonacci number)
-; Pass 'b' as 1 (Second Fibonacci number)
-; To calculate 50th Fibonacci number: '(fib 50 0 1)'
+; To calculate 50th Fibonacci number: '(fib 50)'
 
-(define (fib n a b)
+(define a 0)
+(define b 1)
+(define temp 0)
+
+(define (fib n)
   (cond ((<= n 0) a)
         ((= n 1) b)
-        (else (fib (- n 1) b (+ a b)))
+        (else (and (and (and (set! temp b) (set! b (+ a b)) (set! a temp)) (fib (- n 1) a b))))))
